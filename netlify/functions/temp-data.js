e6a0b6e8a631f1379bd848c02c31a002b8f729f4
Anil Bridgpal <bridgpal@gmail.com>
2023-06-01 15:26:43 -0400

    updated

--- netlify/functions/temp-data.js
+++ netlify/functions/temp-data.js
@@ -1,69 +1,65 @@
-fs = require('fs');
-const aws = require('aws-sdk');
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const fs = require('fs');
 const os = require('os');
 
 let updatedCount;
 let params;
 
-function attemptTmpWriting() {
-  return new Promise((resolve, reject) => {
-    try {
-      const exists = fs.existsSync('/tmp');
-      const fileExists = fs.existsSync('/tmp/counter.txt');
-
-      if (!exists) {
-        fs.mkdirSync('/tmp', {recursive: true});
-      }
-
-      let count = 0;
-      if (fileExists) {
-        const countTxt = fs.readFileSync('/tmp/counter.txt', 'utf-8');
-        if (countTxt !== undefined) {
-          count = parseInt(countTxt, 10);
-        }
-      }
-
-      updatedCount = ++count;
-      let fileName = `/tmp/counter${updatedCount}.txt`;
-      fs.writeFileSync("/tmp/counter.txt", (updatedCount).toString());
-      fs.writeFileSync(fileName, (updatedCount).toString());
-
-      params = {
-        Key: `counter${updatedCount}.txt`,
-        Body: updatedCount.toString(),
-        Bucket: 'anilb-s3',
-        ContentType: 'text/plain'
-      };
-
-      const s3 = new aws.S3({
+const s3Client = new S3Client({
+    region: "us-east-1",  // Update this with your region
+    credentials: {
         accessKeyId: process.env.YOUR_AWS_KEY,
         secretAccessKey: process.env.YOUR_AWS_SECRET
-      });
-
-      s3.putObject(params, function (err, response) {
-        if (err) {
-          console.error(err);
-          reject(err);
-        } else {
-          console.log("file uploaded");
-          resolve();
-        }
-      });
-    } catch (e) {
-      console.error(e);
-      reject(e);
     }
-  });
+});
+
+async function attemptTmpWriting() {
+    try {
+        const exists = fs.existsSync('/tmp');
+        const fileExists = fs.existsSync('/tmp/counter.txt');
+
+        if (!exists) {
+            fs.mkdirSync('/tmp', {recursive: true});
+        }
+
+        let count = 0;
+        if (fileExists) {
+            const countTxt = fs.readFileSync('/tmp/counter.txt', 'utf-8');
+            if (countTxt !== undefined) {
+                count = parseInt(countTxt, 10);
+            }
+        }
+
+        updatedCount = ++count;
+        let fileName = `/tmp/counter${updatedCount}.txt`;
+        fs.writeFileSync("/tmp/counter.txt", (updatedCount).toString());
+        fs.writeFileSync(fileName, (updatedCount).toString());
+
+        params = {
+            Key: `counter${updatedCount}.txt`,
+            Body: updatedCount.toString(),
+            Bucket: 'anilb-s3',
+        };
+
+        const command = new PutObjectCommand(params);
+        await s3Client.send(command);
+
+        console.log("file uploaded");
+
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
 }
 
 exports.handler = async function (event, context) {
-  await attemptTmpWriting();
-  params.folder=os.tmpdir();
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({params}, null, 2)
-  };
-}
+    await attemptTmpWriting();
+    params.folder = os.tmpdir();
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({params}, null, 2)
+    };
+};
