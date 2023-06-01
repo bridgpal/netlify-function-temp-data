58f2f57e1f8a17cdf35a9d4bf8d4325ddb3979a6
Anil Bridgpal <bridgpal@gmail.com>
2023-06-01 14:35:49 -0400

    refactored

--- netlify/functions/temp-data.js
+++ netlify/functions/temp-data.js
@@ -1,43 +1,71 @@
-const fs = require('fs');
+fs = require('fs');
+const aws = require('aws-sdk');
+
+const os = require('os');
+
 
 let updatedCount;
+let params;
 
-function attemptTmpWriting(){
-  try {
+function attemptTmpWriting() {
+  return new Promise((resolve, reject) => {
+    try {
+      const exists = fs.existsSync('/tmp');
+      const fileExists = fs.existsSync('/tmp/counter.txt');
 
-    const exists = fs.existsSync('/tmp');
-    const fileExists = fs.existsSync('/tmp/counter.txt');
-
-    if(!exists){
-      fs.mkdirSync('/tmp', { recursive: true });
-    }
-
-    let count = 0;
-    if (fileExists) {
-      const countTxt = fs.readFileSync('/tmp/counter.txt', 'utf-8');
-      if (countTxt !== undefined) {
-        count = parseInt(countTxt, 10);
+      if (!exists) {
+        fs.mkdirSync('/tmp', {recursive: true});
       }
+
+      let count = 0;
+      if (fileExists) {
+        const countTxt = fs.readFileSync('/tmp/counter.txt', 'utf-8');
+        if (countTxt !== undefined) {
+          count = parseInt(countTxt, 10);
+        }
+      }
+
+      updatedCount = ++count;
+      let fileName = `/tmp/counter${updatedCount}.txt`;
+      fs.writeFileSync("/tmp/counter.txt", (updatedCount).toString());
+      fs.writeFileSync(fileName, (updatedCount).toString());
+
+      params = {
+        Key: `counter${updatedCount}.txt`,
+        Body: updatedCount.toString(),
+        Bucket: 'anilb-s3',
+        ContentType: 'text/plain',
+      };
+
+      const s3 = new aws.S3({
+        accessKeyId: process.env.YOUR_AWS_KEY, 
+        secretAccessKey: process.env.YOUR_AWS_SECRET,
+      });
+
+      s3.putObject(params, function (err, response) {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          console.log("file uploaded");
+          resolve();
+        }
+      });
+    } catch (e) {
+      console.error(e);
+      reject(e);
     }
-
-    updatedCount = ++count;
-    fs.writeFileSync('/tmp/counter.txt', (updatedCount).toString());
-
-  } catch (e) {
-    console.error(e);
-  }
+  });
 }
 
-
 exports.handler = async function (event, context) {
-
-  attemptTmpWriting();
-
+  await attemptTmpWriting();
+  console.log("temp dir",os.tmpdir());
   return {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
-    body: JSON.stringify({updatedCount}, null, 2)
+    body: JSON.stringify({params}, null, 2)
   };
 }
